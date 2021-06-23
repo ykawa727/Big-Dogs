ed6079974b7e24fccb2c9c2d31835f6591350d22
ykawa727 <y.aikawa727@gmail.com>
2021-06-23 14:02:39 +0000

    creating-validations

--- app/models/dog_owner.rb
+++ app/models/dog_owner.rb
@@ -21,5 +21,6 @@ class DogOwner < ApplicationRecord
   validates :postal_code, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
+  validates :introduction, length: { maximum: 500 }
 
 end
