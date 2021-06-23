85b05df0c2af3976c3f869c11358022d067ddf73
ykawa727 <y.aikawa727@gmail.com>
2021-06-23 11:01:47 +0000

    created-ranking_function

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,6 +4,12 @@ class Dog < ApplicationRecord
    has_many :posts, dependent: :destroy
 
    attachment :image
+   
+   validates :dog_category_id, presence: true
+   validates :name, presence: true
+   validates :age, presence: true
+   validates :gender, presence: true
+   validates :introduction, length: { maximum: 500}
 
    enum age: {"０〜１歳（子犬）": 0, "１〜７歳（成犬）": 1, "７〜１０歳（シニア初期）": 2, "１０〜１３歳（シニア中期）": 3, "１３歳以上（シニア後期）": 4}
    enum gender: {"男の子": 0, "女の子": 1, "秘密": 2}
