fb46fa55256949dac337620c3daf5820f6d1b118
ykawa727 <y.aikawa727@gmail.com>
2021-06-24 06:42:38 +0000

    seted-validates

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,10 @@ class Answer < ApplicationRecord
   belongs_to :dog_owner
   belongs_to :post
   has_many :references, dependent: :destroy
-  
+
+  validates :body, presence: true
+  validates :body, length: { maximum: 300}
+
   #「参考になった！」を押した飼い主（ユーザー）の存在を確認するメソッドを定義
   def referenced_by?(dog_owner)
     references.where(dog_owner_id: dog_owner.id).exists?
