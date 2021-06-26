83a16484d1ff874586302fdd528354a72f9afb0c
ykawa727 <y.aikawa727@gmail.com>
2021-06-26 11:27:11 +0000

    fix-errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_dog_owner!,except: [:index, :search, :dog_search]
+  before_action :redirect_if_params_empty, only: :dog_search
 
   def index
     #@posts = Post.page(params[:page]).reverse_order
@@ -32,16 +33,23 @@ class PostsController < ApplicationController
   end
 
   def search
-    @posts = Post.search(params[:category], params[:word])
+    @posts = Post.search(params[:category], params[:word]).page(params[:page])
     render "index"
   end
 
   def dog_search
-    @posts = Post.dog_search(params[:dog_category], params[:age], params[:gender])
+    @posts = Post.dog_search(params[:dog_category], params[:age], params[:gender]).page(params[:page])
     render "index"
   end
 
   private
+  
+  def redirect_if_params_empty
+    if params[:dog_category].blank? && params[:age].blank? && params[:gender].blank?
+      redirect_to root_path, notice: '最低1つの条件を入力して下さい'
+    end
+  end
+  
   def post_params
     params.require(:post).permit(:dog_id, :category, :title, :body, :keyword, :image)
   end
